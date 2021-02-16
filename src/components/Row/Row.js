72fef12acbd0b7d799b3cfc4260395524259b09f
Igor <munkieryder@gmail.com>
2021-02-16 21:06:49 +0100

    unsuccessful movieTrailer npm

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
-import axios from "../../axios"; // import axios from our file aka instance
+import axios from "../../axios";
 
 import "./Row.css";
+
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_URL = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // a snippet of code which runs based on a specific condition/variable
   useEffect(() => {
@@ -18,6 +22,30 @@ function Row({ title, fetchURL, isLargeRow }) {
     }
     fetchData();
   }, [fetchURL]);
+
+  // Youtube options
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  // movieTrailer doesn't work anymore to show trailers for selected movie
+
+  // const handleMovieClick = (movie) => {
+  //   if (trailerUrl) {
+  //     setTrailerUrl("");
+  //   } else {
+  //     movieTrailer(movie?.name || "")
+  //       .then((url) => {
+  //         const urlParams = new URLSearchParams(new URL(url).search);
+  //         setTrailerUrl(urlParams.get("v"));
+  //       })
+  //       .catch((err) => console.log(err));
+  //   }
+  // };
 
   return (
     <div className="row">
@@ -34,9 +62,12 @@ function Row({ title, fetchURL, isLargeRow }) {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.title}
+            // onClick={handleMovieClick(movie)}
           />
         ))}
       </div>
+      {/*  show trailer video IF only there is a trailer  */}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
